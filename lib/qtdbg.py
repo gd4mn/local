42610461dce554acf5d17188679b1adea9f3b6c1
Albert Freeman <albert.freeman@gmail.com>
2025-04-14 05:59:29 -0500

    some optimizations for the debugging library

--- lib/qtdbg.py
+++ lib/qtdbg.py
@@ -75,12 +75,22 @@ class ConsoleMessages:
 console = ConsoleMessages()
 install(show_locals=True)
 
-DEBUG_FILL_STYLE = ""
 if ("-d" in sys.argv) or ("--debug" in sys.argv) or DEBUG:
     DEBUG = True
     LOG_LEVEL = LogLevel.DEBUG
     console.debug("Debug mode enabled")
-    DEBUG_FILL_STYLE = "background-color: rgba(255, 0, 255, 0.2);"
+
+DEBUG_FILL_STYLE = "background-color: rgba(255, 0, 255, 0.2);"
+DEBUG_STYLESHEET = """
+    QWidget {
+        background-color: rgba(255, 0, 255, 0.2);
+        border: 1px solid rgba(0, 0, 0, 0.5);
+    }
+    QLabel {
+        background-color: rgba(0,0,0, 0.25);
+        border: 1px solid rgba(255,255,255,0.2);
+    }
+"""
 
 
 __all__ = [
@@ -88,6 +98,7 @@ __all__ = [
     "DEBUG",
     "LOG_LEVEL",
     "DEBUG_FILL_STYLE",
+    "DEBUG_STYLESHEET",
 ]
 
 if __name__ == "__main__":
