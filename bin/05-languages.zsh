b4782644c55bbc1fd188d2c726f96395568d0b7b
Albert Freeman <albertfreeman@512-Silent-Spark.local>
2025-04-15 16:12:17 -0500

    fixed some issues with pathing

--- bin/05-languages.zsh
+++ bin/05-languages.zsh
@@ -1,4 +1,7 @@
 # install paths and directives for various languages
+# this is early in the process because they may be used
+# later in the process, unlike most programs
 
 export PYTHONPATH="$HOME/.local/lib:$PYTHONPATH"
 typeset -U PYTHONPATH
+export VIRTUALENV_HOME=".venv"
